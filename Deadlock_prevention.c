8cc28ad95b7e4d2d3fc455837e3a6cac44bb2943
Malik Hassan Raza <hassan_leo28@yahoo.com>
2020-05-04 08:08:32 +0500

    Add files via upload
    /*
    Write a program which ask the user to enter the number of processes and the resources required by each process.
    This program also asks for maximum resources required by each process and the allocated and available resources.
    Now it must make sure that it prevents deadlock to occur.  Your program should tell which rule/ condition has been violated if the deadlock occur. 
    */

---
+++ Deadlock_prevention.c
@@ -0,0 +1,155 @@
+/*
+Write a program which ask the user to enter the number of processes and the resources required by each process.
+This program also asks for maximum resources required by each process and the allocated and available resources.
+Now it must make sure that it prevents deadlock to occur.  Your program should tell which rule/ condition has been violated if the deadlock occur. 
+*/
+#include <stdio.h>
+ 
+int current[5][5], maximum_claim[5][5], available[5];
+int allocation[5] = {0, 0, 0, 0, 0};
+int maxres[5], running[5], safe = 0;
+int counter = 0, i, j, exec, resources, processes, k = 1;
+ 
+int main()
+{
+	printf("\nEnter number of processes: ");
+    	scanf("%d", &processes);
+ 
+    	for (i = 0; i < processes; i++) 
+	{
+        	running[i] = 1;
+        	counter++;
+    	}
+ 
+    	printf("\nEnter number of resources: ");
+    	scanf("%d", &resources);
+ 
+    	printf("\nEnter number of instances of each resource, one after the other:\n");
+    	for (i = 0; i < resources; i++) 
+	{ 	printf("R%d: ", i);
+	        scanf("%d", &maxres[i]);
+    	}
+ 
+   	printf("\nEnter Allocated Resource Table:\n");
+    	for (i = 0; i < processes; i++) 
+	{
+		printf("Process %d\n", i);		
+		
+	        for(j = 0; j < resources; j++) 
+		{	printf("AR %d: ", j);
+  			scanf("%d", &current[i][j]);
+        	}
+    	}
+ 
+    	printf("\nEnter MAX resources table:\n");
+    	for (i = 0; i < processes; i++) 
+	{	printf("Process %d\n", i);
+        	for(j = 0; j < resources; j++) 
+		{	printf("MR %d: ", j);
+            		scanf("%d", &maximum_claim[i][j]);
+        	}
+    	}
+ 
+	printf("\nThe Number of Instances of Resources Vector is: ");
+    	for (i = 0; i < resources; i++) 
+	{
+	        printf("\t%d", maxres[i]);
+	}
+ 
+    	printf("\nThe Allocated Resource Table:\n");
+    	for (i = 0; i < processes; i++) 
+	{
+	        for (j = 0; j < resources; j++) 
+		{
+            		printf("\t%d", current[i][j]);
+        	}
+		printf("\n");
+    	}
+ 
+    	printf("\nThe Maximum Resource Table:\n");
+    	for (i = 0; i < processes; i++) 
+	{
+        	for (j = 0; j < resources; j++) 
+		{
+		        printf("\t%d", maximum_claim[i][j]);
+        	}
+        	printf("\n");
+    	}
+ 
+    	for (i = 0; i < processes; i++) 
+	{
+        	for (j = 0; j < resources; j++) 
+		{
+            		allocation[j] += current[i][j];
+        	}
+    	}
+ 
+    	printf("\nAllocated resources:");
+    	for (i = 0; i < resources; i++) 
+	{
+        	printf("\t%d", allocation[i]);
+    	}
+ 
+    	for (i = 0; i < resources; i++) 
+	{
+	        available[i] = maxres[i] - allocation[i];
+	}
+ 
+    	printf("\nAvailable resources:");
+    	for (i = 0; i < resources; i++) 
+	{
+        	printf("\t%d", available[i]);
+    	}
+    	printf("\n");
+ 
+    	while (counter != 0) 
+	{
+        	safe = 0;
+        	for (i = 0; i < processes; i++) 
+		{
+            		if (running[i]) 
+			{
+                		exec = 1;
+                		for (j = 0; j < resources; j++) 
+				{
+                    			if (maximum_claim[i][j] - current[i][j] > available[j]) 
+					{
+                        			exec = 0;
+                        			break;
+                    			}
+                		}
+                		if (exec) 
+				{
+                    			printf("\nProcess %d is executing\n", i + 1);
+                    			running[i] = 0;
+                    			counter--;
+                    			safe = 1;
+ 
+                    			for (j = 0; j < resources; j++) 
+					{
+                        			available[j] += current[i][j];
+                    			}
+			                break;
+                		}
+            		}
+        	}
+        	if (!safe) 
+		{
+            		printf("\nThe processes are in deadlock state.\n");
+            		break;
+        	} 
+		else 
+		{
+            		printf("\nThe process is in safe state");
+            		printf("\nAvailable resources vector:");
+ 
+            		for (i = 0; i < resources; i++) 
+			{
+                		printf("\t%d", available[i]);
+            		}
+ 
+		        printf("\n");
+        	}
+    	}
+    	return 0;
+}
